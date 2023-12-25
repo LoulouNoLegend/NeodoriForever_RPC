9c77c85dbb39ccd20c18c14acd75471118dca83c
LoulouNoLegend <40952934+LoulouNoLegend@users.noreply.github.com>
2023-12-25 17:37:48 -0500

    Base RPC

---
+++ scripts/exports.py
@@ -0,0 +1,13 @@
+import pefile
+
+exported_functions = []
+pe = pefile.PE('.\\steam_api64.dll')
+for entry in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+    func = entry.name.decode('utf-8')
+    exported_functions.append(f'#pragma comment(linker,"/export:{func}=NeodoriForeverRPC.{func},@{entry.ordinal}")')
+
+exported_functions = '\n'.join(exported_functions)
+print(exported_functions)
+
+with open("output.txt", "w") as text_file:
+    text_file.write(exported_functions)
